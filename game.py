56f8005db5473aa1731c827993615973a588d82e
Roshani <dabadi.ro@gmail.com>
2024-03-05 21:26:16 -0800

    Updated game.py

--- game.py
+++ game.py
@@ -1,32 +1,31 @@
-"""A number-guessing game."""
 
 import random
 
 def greet_player():
-    print("Hi!")
-
-def get_player_name():
-    name = input("What's your name? ")
+    print("Howdy, what's your name?")
+    name = input("(type in your name) ")
     return name
 
 def choose_random_number():
     return random.randint(1, 100)
 
 def get_guess():
-    guess = int(input("Guess the number between 1 and 100: "))
+    guess = int(input("Your guess? "))
     return guess
 
 def give_hint(secret_number, player_guess):
     if secret_number < player_guess:
-        print("Too high! Try again.")
+        print("Your guess is too high, try again.")
     else:
-        print("Too low! Try again.")
+        print("Your guess is too low, try again.")
 
 def play_game():
-    greet_player()
-    player_name = get_player_name
+    player_name = greet_player()
     secret_number = choose_random_number()
     num_guesses = 0
+
+    print(f"{player_name}, I'm thinking of a number between 1 and 100.")
+    print("Try to guess my number.")
 
     while True:
         player_guess = get_guess()
@@ -34,15 +33,11 @@ def play_game():
 
         if player_guess != secret_number:
             give_hint(secret_number, player_guess)
-
         else:
-            print(f"Congratulations, {player_name}! You guessed the number in {num_guesses} guesses.")
+            print(f"Well done, {player_name}! You found my number in {num_guesses} tries.")
             break
-    
-#Start the game
+
+# Start the game
 play_game()
-
-
-
 
 
